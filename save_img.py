2d21be5dd6456b79901629a5e438f891428f6758
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2023-05-01 13:43:12 +0300

    some bugs fixed

--- save_img.py
+++ save_img.py
@@ -1,9 +1,30 @@
+import keyboard
 from PIL import ImageGrab
-from PIL import Image
+from datetime import datetime
+import time
+import win32clipboard
 
 
-img = ImageGrab.grabclipboard()
+# shift+win+s kombinasyonu için callback fonksiyonu
+def take_screenshot():
+    time.sleep(2)
+    # Ekran görüntüsünü al
+    img = ImageGrab.grabclipboard()
+    # Ekran görüntüsü alınmışsa kaydet
+    if img:
+        now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        filename = f"screenshot_{now}.png"
+        img.save(filename)
+        print(f"Screenshot saved as {filename}")
+        # Windows panosunu boşalt
+        win32clipboard.OpenClipboard()
+        win32clipboard.EmptyClipboard()
+        win32clipboard.CloseClipboard()
 
 
-if isinstance(img, Image.Image):
-    img.save("screenshot.png")
+# shift+win+s kombinasyonu için hotkey tanımla
+keyboard.add_hotkey("shift+win+s", take_screenshot)
+
+# Sonsuz döngü ile programın çalışmasını sürekli hale getir
+while True:
+    pass
