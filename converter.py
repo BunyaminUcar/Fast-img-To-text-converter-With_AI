c39de6628462c24dc989ac5237422d25f905a78a
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2023-05-06 15:24:24 +0300

    update converter.py for turkish language

--- converter.py
+++ converter.py
@@ -5,7 +5,7 @@ import pytesseract
 class Converter:
     def __init__(self):
         pytesseract.pytesseract.tesseract_cmd = (
-            "C:/Users/UCAR/AppData/Local/Programs/Tesseract-OCR/tesseract.exe"
+            "C:/Program Files/Tesseract-OCR/tesseract.exe"
         )
 
     def convert_img_to_text(self, img_path):
@@ -37,36 +37,19 @@ class Converter:
             dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
         )
 
-        # Creating a copy of image
-        im2 = img.copy()
-
-        # A text file is created and flushed
-        file = open("recognized.txt", "w+")
-        file.write("")
-        file.close()
-
         # Looping through the identified contours
         # Then rectangular part is cropped and passed on
         # to pytesseract for extracting text from it
-        # Extracted text is then written into the text file
-        for cnt in contours:
-            x, y, w, h = cv2.boundingRect(cnt)
+        # Extracted text is then written into a text file
+        with open("recognized.txt", "a", encoding="utf-8") as file:
+            for cnt in contours:
+                x, y, w, h = cv2.boundingRect(cnt)
 
-            # Drawing a rectangle on copied image
-            rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                # Cropping the text block for giving input to OCR
+                cropped = gray[y : y + h, x : x + w]
 
-            # Cropping the text block for giving input to OCR
-            cropped = im2[y : y + h, x : x + w]
+                # Apply OCR on the cropped image
+                text = pytesseract.image_to_string(cropped, lang="tur")
 
-            # Open the file in append mode
-            file = open("result.txt", "w")
-
-            # Apply OCR on the cropped image
-            text = pytesseract.image_to_string(cropped)
-
-            # Appending the text into file
-            file.write(text)
-            file.write("\n")
-
-            # Close the file
-            file.close()
+                # Appending the text into file
+                file.write(text + "\n")
