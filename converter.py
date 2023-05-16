3dec5cdc5c4ec089f83158dfcf817d8ea0bbe2d0
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2023-05-16 17:24:03 +0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -9,47 +9,46 @@ class Converter:
         )
 
     def convert_img_to_text(self, img_path):
-        # Read image from which text needs to be extracted
+        # Metnin çıkarılması gereken görüntüyü okuyun
         img = cv2.imread(img_path)
 
-        # Preprocessing the image starts
+        # Görüntünün ön işlemesine başla
 
-        # Convert the image to gray scale
+        # Görüntüyü gri tonlamaya dönüştürün
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # Performing OTSU threshold
         ret, thresh1 = cv2.threshold(
             gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV
         )
 
-        # Specify structure shape and kernel size.
-        # Kernel size increases or decreases the area
-        # of the rectangle to be detected.
-        # A smaller value like (10, 10) will detect
-        # each word instead of a sentence.
+        # Yapı şeklini ve çekirdek boyutunu belirtin.
+        # Çekirdek boyutu alanı artırır veya azaltır
+        # algılanacak dikdörtgenin sayısı.
+        # (10, 10) gibi daha küçük bir değer algılar
+        # bir cümle yerine her kelime.
         rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
 
-        # Applying dilation on the threshold image
+        # Eşik görüntüsüne dilatasyon uygulama
         dilation = cv2.dilate(thresh1, rect_kernel, iterations=1)
 
-        # Finding contours
+        # Kontur bulma
         contours, hierarchy = cv2.findContours(
             dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
         )
 
-        # Looping through the identified contours
-        # Then rectangular part is cropped and passed on
-        # to pytesseract for extracting text from it
-        # Extracted text is then written into a text file
-        with open("recognized.txt", "a", encoding="utf-8") as file:
+        # Belirlenen konturlar arasında döngü
+        # Daha sonra dikdörtgen kısım kırpılır ve geçilir
+        # ondan metin çıkarmak için pytesseract'a
+        # Ayıklanan metin daha sonra bir metin dosyasına yazılır
+        with open("recognized.txt", "w", encoding="utf-8") as file:
             for cnt in contours:
                 x, y, w, h = cv2.boundingRect(cnt)
 
-                # Cropping the text block for giving input to OCR
+                # OCR'a girdi vermek için metin bloğunun kırpılması
                 cropped = gray[y : y + h, x : x + w]
 
-                # Apply OCR on the cropped image
+                # Kırpılan görüntüye OCR uygulayın
                 text = pytesseract.image_to_string(cropped, lang="tur")
 
-                # Appending the text into file
+                # Metni dosyaya ekleme
                 file.write(text + "\n")
